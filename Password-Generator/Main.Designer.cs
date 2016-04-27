ea74b69849e048c82b8fa7a845d496c18faa877d
Trioxinz <Trioxinz@Trioxinz.com>
2016-04-27 17:16:22 -0400

    Start of development

---
+++ Password-Generator/Main.Designer.cs
@@ -0,0 +1,323 @@
+﻿namespace Password_Generator
+{
+    partial class Main
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.passwordBox = new System.Windows.Forms.RichTextBox();
+            this.passGenButton = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.checkBoxExcludeAmbiguous = new System.Windows.Forms.CheckBox();
+            this.checkBoxExcludeSimilar = new System.Windows.Forms.CheckBox();
+            this.checkBoxIncludeSymbols = new System.Windows.Forms.CheckBox();
+            this.checkBoxIncludeNumbers = new System.Windows.Forms.CheckBox();
+            this.checkBoxIncludeUpperCase = new System.Windows.Forms.CheckBox();
+            this.checkBoxIncludeLowerCase = new System.Windows.Forms.CheckBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // passwordBox
+            // 
+            this.passwordBox.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.passwordBox.Location = new System.Drawing.Point(280, 12);
+            this.passwordBox.Name = "passwordBox";
+            this.passwordBox.ReadOnly = true;
+            this.passwordBox.Size = new System.Drawing.Size(292, 417);
+            this.passwordBox.TabIndex = 0;
+            this.passwordBox.Text = "";
+            // 
+            // passGenButton
+            // 
+            this.passGenButton.Location = new System.Drawing.Point(81, 193);
+            this.passGenButton.Name = "passGenButton";
+            this.passGenButton.Size = new System.Drawing.Size(120, 32);
+            this.passGenButton.TabIndex = 1;
+            this.passGenButton.Text = "Generate Password";
+            this.passGenButton.UseVisualStyleBackColor = true;
+            this.passGenButton.Click += new System.EventHandler(this.passGenButton_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.comboBox1);
+            this.panel1.Controls.Add(this.checkBoxExcludeAmbiguous);
+            this.panel1.Controls.Add(this.checkBoxIncludeLowerCase);
+            this.panel1.Controls.Add(this.checkBoxExcludeSimilar);
+            this.panel1.Controls.Add(this.checkBoxIncludeSymbols);
+            this.panel1.Controls.Add(this.checkBoxIncludeUpperCase);
+            this.panel1.Controls.Add(this.checkBoxIncludeNumbers);
+            this.panel1.Location = new System.Drawing.Point(12, 12);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(262, 175);
+            this.panel1.TabIndex = 2;
+            // 
+            // checkBoxExcludeAmbiguous
+            // 
+            this.checkBoxExcludeAmbiguous.AutoSize = true;
+            this.checkBoxExcludeAmbiguous.Location = new System.Drawing.Point(6, 144);
+            this.checkBoxExcludeAmbiguous.Name = "checkBoxExcludeAmbiguous";
+            this.checkBoxExcludeAmbiguous.Size = new System.Drawing.Size(247, 17);
+            this.checkBoxExcludeAmbiguous.TabIndex = 12;
+            this.checkBoxExcludeAmbiguous.Text = "Exclude Ambiguous  { } [ ] ( ) / \\ \' \" ` ~ , ; : . < >";
+            this.checkBoxExcludeAmbiguous.UseVisualStyleBackColor = true;
+            // 
+            // checkBoxExcludeSimilar
+            // 
+            this.checkBoxExcludeSimilar.AutoSize = true;
+            this.checkBoxExcludeSimilar.Location = new System.Drawing.Point(6, 121);
+            this.checkBoxExcludeSimilar.Name = "checkBoxExcludeSimilar";
+            this.checkBoxExcludeSimilar.Size = new System.Drawing.Size(256, 17);
+            this.checkBoxExcludeSimilar.TabIndex = 11;
+            this.checkBoxExcludeSimilar.Text = "Exclude Similar Characters           i, l, 1, L, o, 0, O";
+            this.checkBoxExcludeSimilar.UseVisualStyleBackColor = true;
+            // 
+            // checkBoxIncludeSymbols
+            // 
+            this.checkBoxIncludeSymbols.AutoSize = true;
+            this.checkBoxIncludeSymbols.Location = new System.Drawing.Point(6, 98);
+            this.checkBoxIncludeSymbols.Name = "checkBoxIncludeSymbols";
+            this.checkBoxIncludeSymbols.Size = new System.Drawing.Size(237, 17);
+            this.checkBoxIncludeSymbols.TabIndex = 10;
+            this.checkBoxIncludeSymbols.Text = "Include Symbols:                              @#!$%^";
+            this.checkBoxIncludeSymbols.UseVisualStyleBackColor = true;
+            // 
+            // checkBoxIncludeNumbers
+            // 
+            this.checkBoxIncludeNumbers.AutoSize = true;
+            this.checkBoxIncludeNumbers.Location = new System.Drawing.Point(6, 75);
+            this.checkBoxIncludeNumbers.Name = "checkBoxIncludeNumbers";
+            this.checkBoxIncludeNumbers.Size = new System.Drawing.Size(226, 17);
+            this.checkBoxIncludeNumbers.TabIndex = 9;
+            this.checkBoxIncludeNumbers.Text = "Include Numbers                              123... ";
+            this.checkBoxIncludeNumbers.UseVisualStyleBackColor = true;
+            // 
+            // checkBoxIncludeUpperCase
+            // 
+            this.checkBoxIncludeUpperCase.AutoSize = true;
+            this.checkBoxIncludeUpperCase.Location = new System.Drawing.Point(6, 53);
+            this.checkBoxIncludeUpperCase.Name = "checkBoxIncludeUpperCase";
+            this.checkBoxIncludeUpperCase.Size = new System.Drawing.Size(227, 17);
+            this.checkBoxIncludeUpperCase.TabIndex = 8;
+            this.checkBoxIncludeUpperCase.Text = "Include Uppercase Characters         ABC...";
+            this.checkBoxIncludeUpperCase.UseVisualStyleBackColor = true;
+            // 
+            // checkBoxIncludeLowerCase
+            // 
+            this.checkBoxIncludeLowerCase.AutoSize = true;
+            this.checkBoxIncludeLowerCase.Location = new System.Drawing.Point(6, 30);
+            this.checkBoxIncludeLowerCase.Name = "checkBoxIncludeLowerCase";
+            this.checkBoxIncludeLowerCase.Size = new System.Drawing.Size(224, 17);
+            this.checkBoxIncludeLowerCase.TabIndex = 7;
+            this.checkBoxIncludeLowerCase.Text = "Include Lowercase Characters         abc...";
+            this.checkBoxIncludeLowerCase.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(3, 6);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(43, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Length:";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "6",
+            "7",
+            "8",
+            "9",
+            "10",
+            "11",
+            "12",
+            "13",
+            "14",
+            "15",
+            "16",
+            "17",
+            "18",
+            "19",
+            "20",
+            "21",
+            "22",
+            "23",
+            "24",
+            "25",
+            "26",
+            "27",
+            "28",
+            "29",
+            "30",
+            "31",
+            "32",
+            "33",
+            "34",
+            "35",
+            "36",
+            "37",
+            "38",
+            "39",
+            "40",
+            "41",
+            "42",
+            "43",
+            "44",
+            "45",
+            "46",
+            "47",
+            "48",
+            "49",
+            "50",
+            "51",
+            "52",
+            "53",
+            "54",
+            "55",
+            "56",
+            "57",
+            "58",
+            "59",
+            "60",
+            "61",
+            "62",
+            "63",
+            "64",
+            "65",
+            "66",
+            "67",
+            "68",
+            "69",
+            "70",
+            "71",
+            "72",
+            "73",
+            "74",
+            "75",
+            "76",
+            "77",
+            "78",
+            "79",
+            "80",
+            "81",
+            "82",
+            "83",
+            "84",
+            "85",
+            "86",
+            "87",
+            "88",
+            "89",
+            "90",
+            "91",
+            "92",
+            "93",
+            "94",
+            "95",
+            "96",
+            "97",
+            "98",
+            "99",
+            "100",
+            "101",
+            "102",
+            "103",
+            "104",
+            "105",
+            "106",
+            "107",
+            "108",
+            "109",
+            "110",
+            "111",
+            "112",
+            "113",
+            "114",
+            "115",
+            "116",
+            "117",
+            "118",
+            "119",
+            "120",
+            "121",
+            "122",
+            "123",
+            "124",
+            "125",
+            "126",
+            "127",
+            "128",
+            "256",
+            "512",
+            "1024",
+            "2048",
+            "4096",
+            "8192",
+            "16384"});
+            this.comboBox1.Location = new System.Drawing.Point(52, 3);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(94, 21);
+            this.comboBox1.TabIndex = 0;
+            // 
+            // Main
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(584, 441);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.passGenButton);
+            this.Controls.Add(this.passwordBox);
+            this.Name = "Main";
+            this.Text = "Main";
+            this.Load += new System.EventHandler(this.Main_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox passwordBox;
+        private System.Windows.Forms.Button passGenButton;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.CheckBox checkBoxExcludeAmbiguous;
+        private System.Windows.Forms.CheckBox checkBoxExcludeSimilar;
+        private System.Windows.Forms.CheckBox checkBoxIncludeSymbols;
+        private System.Windows.Forms.CheckBox checkBoxIncludeNumbers;
+        private System.Windows.Forms.CheckBox checkBoxIncludeUpperCase;
+        private System.Windows.Forms.CheckBox checkBoxIncludeLowerCase;
+    }
+}
---
+++ Password-Generator/Main.cs
@@ -0,0 +1,172 @@
+﻿using System;
+using System.Collections;
+using System.Windows.Forms;
+
+namespace Password_Generator
+{
+    public partial class Main : Form
+    {
+        static Random rnd = new Random();
+
+        public Main()
+        {
+            InitializeComponent();
+        }
+
+        private void Main_Load(object sender, EventArgs e)
+        {
+            comboBox1.SelectedIndex = 0;
+            checkBoxIncludeLowerCase.Checked = true;
+        }
+
+        private void passGenButton_Click(object sender, EventArgs e)
+        {
+            if (!checkBoxIncludeLowerCase.Checked && !checkBoxIncludeUpperCase.Checked && !checkBoxIncludeSymbols.Checked && !checkBoxIncludeNumbers.Checked)
+            {
+                MessageBox.Show("Error", "You need to select a type of character to include.", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+            string password = "";
+            int length = int.Parse(comboBox1.SelectedItem.ToString());
+            ArrayList array = new ArrayList();
+            if (checkBoxIncludeLowerCase.Checked)
+            {
+                array.Add("a");
+                array.Add("b");
+                array.Add("c");
+                array.Add("d");
+                array.Add("e");
+                array.Add("f");
+                array.Add("g");
+                array.Add("h");
+                array.Add("i");
+                array.Add("j");
+                array.Add("k");
+                array.Add("l");
+                array.Add("m");
+                array.Add("n");
+                array.Add("o");
+                array.Add("p");
+                array.Add("q");
+                array.Add("r");
+                array.Add("s");
+                array.Add("t");
+                array.Add("u");
+                array.Add("v");
+                array.Add("w");
+                array.Add("x");
+                array.Add("y");
+                array.Add("z");
+            }
+            if (checkBoxIncludeUpperCase.Checked)
+            {
+                array.Add("A");
+                array.Add("B");
+                array.Add("C");
+                array.Add("D");
+                array.Add("E");
+                array.Add("F");
+                array.Add("G");
+                array.Add("H");
+                array.Add("I");
+                array.Add("J");
+                array.Add("K");
+                array.Add("L");
+                array.Add("M");
+                array.Add("N");
+                array.Add("O");
+                array.Add("P");
+                array.Add("Q");
+                array.Add("R");
+                array.Add("S");
+                array.Add("T");
+                array.Add("U");
+                array.Add("V");
+                array.Add("W");
+                array.Add("X");
+                array.Add("Y");
+                array.Add("Z");
+            }
+            if (checkBoxIncludeNumbers.Checked)
+            {
+                array.Add("1");
+                array.Add("2");
+                array.Add("3");
+                array.Add("4");
+                array.Add("5");
+                array.Add("6");
+                array.Add("7");
+                array.Add("8");
+                array.Add("9");
+                array.Add("0");
+            }
+            if (checkBoxIncludeSymbols.Checked)
+            {
+                array.Add("~");
+                array.Add("`");
+                array.Add("!");
+                array.Add("@");
+                array.Add("#");
+                array.Add("$");
+                array.Add("%");
+                array.Add("^");
+                array.Add("&");
+                array.Add("*");
+                array.Add("(");
+                array.Add(")");
+                array.Add("_");
+                array.Add("=");
+                array.Add("+");
+                array.Add("{");
+                array.Add("}");
+                array.Add("[");
+                array.Add("]");
+                array.Add("|");
+                array.Add("\\"); //need double \ for the string to take the \ char 
+                array.Add("/");
+                array.Add("?");
+                array.Add("<");
+                array.Add(">");
+                array.Add(",");
+                array.Add(".");
+                array.Add(";");
+                array.Add(":");
+            }
+            if (checkBoxExcludeSimilar.Checked)
+            {
+                array.Remove("i");
+                array.Remove("1");
+                array.Remove("l");
+                array.Remove("L");
+                array.Remove("o");
+                array.Remove("O");
+                array.Remove("0");
+            }
+            if (checkBoxExcludeAmbiguous.Checked)
+            {
+                array.Remove("{");
+                array.Remove("}");
+                array.Remove("[");
+                array.Remove("]");
+                array.Remove("(");
+                array.Remove(")");
+                array.Remove("\\");
+                array.Remove("/");
+                array.Remove("`");
+                array.Remove("~");
+                array.Remove(",");
+                array.Remove(";");
+                array.Remove(":");
+                array.Remove(".");
+                array.Remove("<");
+                array.Remove(">");
+            }
+            for (int i = 0; i < length; i++)
+            {
+                int j = rnd.Next(array.Count);
+                password = password + array[j].ToString();
+            }
+            passwordBox.Text = password;
+        }
+    }
+}
