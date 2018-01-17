5d9573baf8ca7b02fca67e805a5a7316feed71e2
Trioxinz <Trioxinz@users.noreply.github.com>
2018-01-17 11:34:49 -0500

    Update building configurations

--- Password-Generator/Properties/AssemblyInfo.cs
+++ Password-Generator/Properties/AssemblyInfo.cs
@@ -1,16 +1,17 @@
-﻿using System.Reflection;
+﻿using System.Resources;
+using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Password-Generator")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle("Password Generator")]
+[assembly: AssemblyDescription("A password generation tool.")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Password-Generator")]
-[assembly: AssemblyCopyright("Copyright ©  2016")]
+[assembly: AssemblyProduct("Password Generator")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -34,3 +35,5 @@ using System.Runtime.InteropServices;
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: NeutralResourcesLanguage("en")]
+
